b3019eccb41274f54656410c52b86854d5bec89d
matins <56564831+matins85@users.noreply.github.com>
2020-08-14 15:17:08 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -99,10 +99,10 @@ function buildAndShowHomeHTML (categories) {
     function (homeHtml) {
 
       
-      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
+      let chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
       chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
+      let homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
       insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
